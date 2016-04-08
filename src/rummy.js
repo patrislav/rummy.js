e93f38bf316863c8c79453a5b574fedd85074e4d
Patryk Kalinowski <patrislav7@gmail.com>
2016-04-08 12:29:57 +0200

    Add Rummy.options

--- src/rummy.js
+++ src/rummy.js
@@ -1,8 +1,36 @@
 
 import { version } from '../package.json';
 
+const _opts = Symbol('opts');
+
 export class Rummy {
   static version = version;
+
+  constructor() {
+    this[_opts] = {};
+  }
+
+  /**
+   * @param {Object|String} keyOrOpts One key to get that option or an object
+   * with options to set
+   * @return {Object|String} All options if called without arguments, or one
+   * value when used with key
+   */
+  options(keyOrOpts) {
+    let opts = this[_opts];
+
+    if (!keyOrOpts) {
+      return opts;
+    }
+
+    if (typeof keyOrOpts === 'string') {
+      return (keyOrOpts in opts) ? opts[keyOrOpts] : null;
+    }
+
+    for (let i in keyOrOpts) {
+      opts[i] = keyOrOpts[i];
+    }
+  }
 }
 
 export default Rummy;
---
+++ test/options.js
@@ -0,0 +1,42 @@
+import { expect } from 'chai';
+
+import Rummy from '..';
+
+describe('Rummy.options', () => {
+
+  describe('when called without arguments', () => {
+    it('returns default options', () => {
+      let rummy = new Rummy();
+      expect(rummy.options())
+        .to.be.an('object');
+    });
+  });
+
+  describe('when called with an object', () => {
+    it('sets the options', () => {
+      let rummy = new Rummy();
+      rummy.options({ option1: 'value1', option2: 'value2' });
+      expect(rummy.options('option1'))
+        .to.be.equal('value1');
+      expect(rummy.options('option2'))
+        .to.be.equal('value2');
+    });
+
+    it("doesn't change the previously set options", () => {
+      let rummy = new Rummy();
+      rummy.options({ option1: 'value1', option2: 'value2' });
+      rummy.options({ option3: 'value3' });
+      expect(rummy.options('option1'))
+        .to.be.equal('value1');
+    });
+
+    it('changes the previously set option if it was provided', () => {
+      let rummy = new Rummy();
+      rummy.options({ option1: 'value1', option2: 'value2' });
+      rummy.options({ option1: 'changed-value' });
+      expect(rummy.options('option1'))
+        .to.be.equal('changed-value');
+    });
+  });
+
+});
