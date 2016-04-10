e966c3f70710c8155cc7d19ef52aceb27736471f
Patryk Kalinowski <patrislav7@gmail.com>
2016-04-10 16:39:54 +0200

    Add card decorators

---
+++ src/decorators.js
@@ -0,0 +1,12 @@
+
+export function CardConstants(target) {
+  target.SPADES = 0;
+  target.HEARTS = 1;
+  target.DIAMONDS = 2;
+  target.CLUBS = 3;
+
+  target.ACE = 1;
+  target.JACK = 11;
+  target.QUEEN = 12;
+  target.KING = 13;
+}
